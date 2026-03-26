4b80b50d2c75f9a656671ae77b6fd13a45dc2ff1
LuisGrigore <LuisGrigore@users.noreply.github.com>
2026-03-26 15:49:37 +0100

    ML model refactor.

--- back/model.py
+++ back/model.py
@@ -2,21 +2,18 @@ import cv2
 import tensorflow as tf
 import numpy as np
 
-
-
 class Model:
     def __init__(self,model_path):
-        self.model = None
-        self.load_model(model_path)
-
-    def load_model(self, model_path):
         try:
             self.model = tf.keras.models.load_model(model_path)
         except Exception:
             raise Exception("model couldn't be loaded")
 
     def classify(self, image_path):
-        img = cv2.imread(image_path)[:, :, 0]
+        img = cv2.imread(image_path)
+        if img is None:
+            raise Exception("image could not be read.")
+        img = img[:, :, 0]
         img = np.invert(np.array([img]))
         img = img / 255
         prediction = self.model.predict(img)
